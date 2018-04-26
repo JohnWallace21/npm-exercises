0422d0c8e2d352b80cdcf2ec2a16b15f32e41f0e
John Wallace <jnwywe@gmail.com>
2018-04-26 11:56:30 -0500

    added public directory

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const $ = require('jquery');
+
+const sayHello = () => console.log("Hello");
+
+
+
+sayHello();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("body").css("background-color", "salmon");
+
+$("body").append("<h1>Happy NPM Day, everybody!</h1>");
