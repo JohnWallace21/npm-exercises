107a8d5908ceb683b7450510736e04190a24e7e9
John Wallace <jnwywe@gmail.com>
2018-04-26 14:18:20 -0500

    finished npm exercise

--- src/index.js
+++ src/index.js
@@ -1,36 +1,12 @@
 const $ = require('jquery');
 
-const sayHello = () => console.log("Hello");
+const sayHello = () => {
+    console.log("Hello");
+};
 
 
 
 sayHello();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 $("body").css("background-color", "salmon");
 
